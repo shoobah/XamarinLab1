b2160111b6954b2fe564af9fecb80b3d9868b0f3
Anders Sjöberg <shoobah@gmail.com>
2016-04-02 01:57:19 +0200

    First

--- App1/Resources/Resource.Designer.cs
+++ App1/Resources/Resource.Designer.cs
@@ -60,8 +60,14 @@ namespace App1
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int address_text = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int get_address_button = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int location_text = 2131034112;
 			
 			static Id()
 			{
